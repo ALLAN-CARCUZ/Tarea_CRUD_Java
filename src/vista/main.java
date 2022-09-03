959260f251d8b19f001ba471c30a0179cc6a9045
ALLAN-CARCUZ <allancarcuz@gmail.com>
2022-09-03 17:56:38 -0600

    Entrega_tarea

---
+++ src/vista/main.java
@@ -0,0 +1,13 @@
+package vista;
+
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        frm_cliente frm = new frm_cliente();
+        frm.show();
+        
+    }
+    
+}
